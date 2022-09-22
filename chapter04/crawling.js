83dec47c648451cba18c1552e79920c3d8746cdc
jwh926 <tridath3@gmail.com>
2022-09-22 17:01:06 +0900

    crawling part

---
+++ chapter04/crawling.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getHtml = async () => {
+	try{
+		return await axios.get(
+			'https://roadbook.co.kr/category/%EC%8B%A0%EA%B0%84%EC%86%8C%EA%B0%9C'
+		);
+	} catch(err) {
+		console.error(err);
+	}
+};
+
+getHtml().then(html => {
+	let ulList = [];
+	const $ = cheerio.load(html.data);
+	const $bodyList = $("div#searchList ol").children("li");
+
+	$bodyList.each(function(i, elem) {
+		ulList[i] = {
+			bookList: $(this).find('a').text(),
+			url: $(this).find('a').attr('href'),
+		};
+		const data = ulList.filter(n => n.bookList);
+		return data;
+	});
+}).then(res => console.log(res));
