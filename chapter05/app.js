59f7cefcd16cb760520a72377666108bc74756f5
jwh926 <tridath3@gmail.com>
2022-09-27 16:02:18 +0900

    chapter05

--- chapter05/app.js
+++ chapter05/app.js
@@ -1,11 +1,40 @@
-const {sequelize} = require('./models/index.js');
+const morgan = require('morgan');
+const models = require('./models');
+const express = require('express')
+const app = express()
 
-const driver = () => {
-	sequelize.sync().then(() => {
-		console.log('초기화 완료')
+app.set('port', process.env.PORT || 8080);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res, next) => {
+	models.newCustomer.findAll().then((customers) => {
+		res.send(customers);
 	}).catch((err) => {
-		console.error('초기화 실패');
 		console.error(err);
+		next(err);
 	});
-};
-driver();
+});
+
+app.get('/customer', (req, res) => {
+	res.sendFile(__dirname + '/customer.html');
+});
+
+app.post('/customer', (req, res) => {
+	let body = req.body;
+
+	models.newCustomer.create({
+		name: body.name,
+		age: body.age,
+		sex: body.sex
+	}).then(result => {
+		console.log('customer created!');
+		res.redirect('/customer');
+	}).catch(err => {
+		console.log(err);
+	});
+});
+
+app.listen(app.get('port'), () => console.log(`http://localhost:${app.get('port')}`));
